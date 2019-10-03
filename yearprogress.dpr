46a27009dd3946d53232017fc9c67285c17189f7
Dmit Computers Development <44694825+dmitryevdev@users.noreply.github.com>
2019-10-03 19:22:47 +0700

    Update yearprogress.dpr

--- yearprogress.dpr
+++ yearprogress.dpr
@@ -2,7 +2,8 @@ program yearprogress;
 
 uses
   Vcl.Forms,
-  YearProgressForm in 'YearProgressForm.pas' {YearProgressForm1};
+  YearProgressForm in 'YearProgressForm.pas' {YearProgressForm1},
+  AboutProgram in 'AboutProgram.pas' {AboutProgram1};
 
 {$R *.res}
 
@@ -10,5 +11,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TYearProgressForm1, YearProgressForm1);
+  Application.CreateForm(TAboutProgram1, AboutProgram1);
   Application.Run;
 end.
